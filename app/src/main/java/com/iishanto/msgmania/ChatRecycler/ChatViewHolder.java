15e996f6e1c2524d07872c468411642513197c15
Sharif Hasan <me.sharif.hasan@gmail.com>
2021-09-24 20:21:19 +0600

    Going to ubuntu

--- app/src/main/java/com/iishanto/msgmania/ChatRecycler/ChatViewHolder.java
+++ app/src/main/java/com/iishanto/msgmania/ChatRecycler/ChatViewHolder.java
@@ -7,7 +7,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.iishanto.msgmania.ChatActivity;
 import com.iishanto.msgmania.R;
 import com.iishanto.msgmania.Util;
